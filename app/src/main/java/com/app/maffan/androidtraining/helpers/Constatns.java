d0b7de446a0e491e83dd202a08081f3a6a455c96
Muhammad Affan Khan <maffan@folio3.com>
2017-05-16 15:33:59 +0500

    added constant class and some refactoring

---
+++ app/src/main/java/com/app/maffan/androidtraining/helpers/Constatns.java
@@ -0,0 +1,10 @@
+package com.app.maffan.androidtraining.helpers;
+
+/**
+ * Created by maffan on 5/16/17.
+ */
+
+public class Constatns {
+
+    public static final String RANDOM_USER_BASE_URL = "https://randomuser.me";
+}
--- app/src/main/java/com/app/maffan/androidtraining/network/ServiceGenerator.java
+++ app/src/main/java/com/app/maffan/androidtraining/network/ServiceGenerator.java
@@ -2,22 +2,19 @@ package com.app.maffan.androidtraining.network;
 
 
 import com.google.gson.Gson;
-
+import com.app.maffan.androidtraining.helpers.Constatns;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
 
-
-    private static String BASE_URL = "https://randomuser.me";
     private static Retrofit retrofit = null;
-
     public static Retrofit getRetrofit(){
 
         if(retrofit == null){
 
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(Constatns.RANDOM_USER_BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
